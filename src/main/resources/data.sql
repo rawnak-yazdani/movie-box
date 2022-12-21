da02aec14ea6745798b4680f79c864a9ec207808
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-12-21 11:23:35 +0600

    added data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO genre (id, name) VALUES (nextval('genre_seq'), 'DRAMA');
+-- INSERT INTO genre (id, name) VALUES (nextval('genre_seq'), 'DRAMA');
+INSERT INTO app_user (id, name, password, role, usercreationdate, username) VALUES (nextval('user_seq'), 'General Akbur', '$2a$12$Ipl1UomhdKyXuvqxONgHduzspUycq6MWg9Ve9OfpUNlQAr4m71B9y', 'admin', now(), 'dictator');
