6032fba6b7311e73cd280cc5aaf12cb140f1f475
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-12-25 11:19:25 +0600

    Changed Redis cache to Spring boot Cache

--- src/main/java/io/welldev/initializer/configuration/CacheConfig.java
+++ src/main/java/io/welldev/initializer/configuration/CacheConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCust
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,19 +22,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@ComponentScan({"io.welldev.initializer.configuration.userauth"})
 public class CacheConfig {
     public final static String BLACKLIST_CACHE_NAME = "jwt-black-list";
 
-    @Bean
-    RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
-        Long lifeTime = Long.parseLong(System.getenv("TOKEN_EXPIRE_TIME"));
-        return (builder) -> builder
-                    .withCacheConfiguration(BLACKLIST_CACHE_NAME,
-                            RedisCacheConfiguration
-                                    .defaultCacheConfig()
-                                    .entryTtl(Duration.ofHours(lifeTime)));
+//    @Bean
+//    RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+//        Long lifeTime = Long.parseLong(System.getenv("TOKEN_EXPIRE_TIME"));
+//        return (builder) -> builder
+//                    .withCacheConfiguration(BLACKLIST_CACHE_NAME,
+//                            RedisCacheConfiguration
+//                                    .defaultCacheConfig()
+//                                    .entryTtl(Duration.ofHours(lifeTime)));
+//
+//    }
 
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager(BLACKLIST_CACHE_NAME);
     }
 
 }
